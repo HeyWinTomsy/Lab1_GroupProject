a9f0b456dac75b585dd6d070f2f29931eae3b08f
HeyWinTomsy <nazarantr@gmail.com>
2026-09-23 17:37:36 +0300

    Implement student A algorithm

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include <memory>
+#include "shared_types.h"
+
+std::unique_ptr<Result> calculateA(std::shared_ptr<const InputData> data);
+
 int main()
 {
+    auto data = std::make_shared<const InputData>(InputData{0.8, 1.0, 0.1, 10});
+    auto resultA = calculateA(data);          // <-- виклик Студента А
+    auto [valueA, exactA, errorA, itA, nameA] = *resultA;
+    std::cout << nameA << ": y = " << valueA << std::endl;
     return 0;
 }
